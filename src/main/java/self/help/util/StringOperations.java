1360a2764cf6175a61ac52c89e4f297e2bd0d2f0
vsalso <vsalso@gmail.com>
2022-12-11 10:17:29 +0530

    Adding util file

---
+++ src/main/java/self/help/util/StringOperations.java
@@ -0,0 +1,17 @@
+package self.help.util;
+
+import java.util.Date;
+
+public class StringOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static String dateToString(Date inputDate) {
+		String convertedDate = "12/12/2022";
+		return convertedDate;
+	}
+
+}
